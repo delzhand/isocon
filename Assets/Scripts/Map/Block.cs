0fef3dbfb8adf81ba5f14b5d25325f270b6ef0af
delzhand <delzhand@gmail.com>
2023-07-30 18:58:03 -0400

    fix clickthrough to blocks

--- Assets/Scripts/Map/Block.cs
+++ Assets/Scripts/Map/Block.cs
@@ -141,6 +141,9 @@ public class Block : MonoBehaviour
 
     void OnMouseDown()
     {
+        if (UI.InElement("EditTokenPanel")) {
+            return;
+        }
         if (UI.ClicksSuspended) {
             return;
         }
