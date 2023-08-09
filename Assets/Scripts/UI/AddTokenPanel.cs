8a074e6747b95f0249077677fe79f099565445fd
delzhand <delzhand@gmail.com>
2023-08-08 23:39:08 -0400

    checkpoint

--- Assets/Scripts/UI/AddTokenPanel.cs
+++ Assets/Scripts/UI/AddTokenPanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Mirror;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -7,12 +8,31 @@ using UnityEngine.UIElements;
 public class AddTokenPanel : MonoBehaviour
 {
     void Awake() {
-        // UI.System.Q<Button>("AddTokenCreateButton").RegisterCallback<ClickEvent>((evt) => {
-        //     Player p = NetworkClient.localPlayer.GetComponent<Player>();
-        //     string name = UI.System.Q<TextField>("TokenNameField").value;
-        //     string jclass = UI.System.Q<DropdownField>("ClassDropdown").value;
-        //     string job = UI.System.Q<DropdownField>("JobDropdown").value;
-        //     p.CmdRequestAddToken(name, job, jclass);
-        // });
+        UI.System.Q<Button>("AddTokenButton").RegisterCallback<ClickEvent>((evt) => {
+            UI.ToggleDisplay("AddTokenPanel");
+        });
+
+        string path = PlayerPrefs.GetString("DataFolder", Application.persistentDataPath);
+        UI.System.Q<Label>("DataPath").text = "Graphics for tokens should be placed in " + path + "/tokens/";
+
+        List<string> customGraphics = GetCustomGraphics();
+        UI.System.Q<DropdownField>("GraphicDropdown").choices = customGraphics;
+        UI.System.Q<DropdownField>("GraphicDropdown").value = customGraphics[0];
+    }
+
+    private List<string> GetCustomGraphics() {
+        string path = PlayerPrefs.GetString("DataFolder", Application.persistentDataPath);
+        List<string> graphics = new() { };
+        DirectoryInfo info = new(path + "/tokens/");
+        if (info.Exists) {
+            FileInfo[] fileInfo = info.GetFiles();
+            for (int i = 0; i < fileInfo.Length; i++) {
+                graphics.Add(fileInfo[i].Name);
+                if (i == 0) {
+                    UI.System.Q<DropdownField>("GraphicDropdown").value = fileInfo[i].Name;
+                }
+            }        
+        }
+        return graphics;
     }
 }
