7487eddaba94684a99bd6d44a6552fc787060a76
delzhand <delzhand@gmail.com>
2023-11-02 23:06:49 -0400

    more maleghast progress

--- Assets/Scripts/GameSystems/Maleghast/MaleghastTokenData.cs
+++ Assets/Scripts/GameSystems/Maleghast/MaleghastTokenData.cs
@@ -66,9 +66,10 @@ public class MaleghastTokenData : TokenData
 
     public int Size;
 
-    public string Ability1;
-    public string Ability2;
-    public string Ability3;
+    public string ActAbilities;
+    public string SoulAbilities;
+    public string Upgrades;
+    public string Traits;
 
     public int Strength;
     public int Weak;
@@ -92,6 +93,10 @@ public class MaleghastTokenData : TokenData
     public override void UpdateUIData() {
         overhead.Q<ProgressBar>("HpBar").value = CurrentHP;
         overhead.Q<ProgressBar>("HpBar").highValue = MaxHP;
+        UI.ToggleDisplay(overhead.Q<ProgressBar>("VigorBar"), false);
+        for (int i = 1; i <= 3; i++) {
+            UI.ToggleDisplay(overhead.Q("Wound" + i), false);            
+        }
     }
 
     public override void TokenDataSetup(string json) {
@@ -176,10 +181,378 @@ public class MaleghastTokenData : TokenData
     }
 
     private void SetStats(string unitType) {
-        
+        switch (unitType) {
+            case "Gunwight/Thrall":
+                Move = 2;
+                MaxHP = 2;
+                Defense = 4;
+                Armor = "";
+                Traits = "Formation,Thrall";
+                ActAbilities = "OL45,Baton";
+                Upgrades = "Brace,Tactical Reload,Scavenge Ammo";
+                break;
+            case "Enforcer/Scion":
+                Move = 3;
+                MaxHP = 4;
+                Defense = 3;
+                Armor = "ARMOR";
+                Traits = "Formation";
+                ActAbilities = "Skull Crack,Flashbang,Shieldwall";
+                Upgrades = "Shield Charge,Bulwark,Bone Dust Napalm";
+                break;
+            case "Ammo Goblin/Freak":
+                Move = 3;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Formation";
+                ActAbilities = "Regurgitate Ammo,Bone Shards,Destructive Glee";
+                Upgrades = "Vomit Bullets,Napalm Injector,Hot Chamber";
+                break;
+            case "Barrelform/Hunter":
+                Move = 2;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Formation";
+                ActAbilities = "Snipe,Transform To Gun,Deathmark";
+                Upgrades = "Claw Pitons,Extended Barrel,Caliber Up";
+                break;
+            case "Aegis Weapon/Tyrant":
+                Move = 3;
+                MaxHP = 6;
+                Defense = 4;
+                Armor = "ARMOR";
+                Traits = "Formation,Siege Shield,Tyrant";
+                ActAbilities = "Juggernaut,Mortar,Catechism Devil Cannon";
+                Upgrades = "Fortify,Heavy Caliber Cannon,Gunner Pivot";
+                break;
+            case "Operator/Necromancer":
+                Move = 4;
+                MaxHP = 8;
+                Defense = 4;
+                Armor = "ARMOR";
+                Traits = "Formation,Hot Clip";
+                ActAbilities = "Akimbo";
+                SoulAbilities = "Reload Slide,Bullet Time";
+                break;
+            case "Warhead/Thrall":
+                Move = 4;
+                MaxHP = 2;
+                Defense = 3;
+                Armor = "";
+                Traits = "Blood Rage,Thrall";
+                ActAbilities = "Charge,Cleave";
+                Upgrades = "Bladed,Overclocked,Lobotomized";
+                break;
+            case "Carnifex/Scion":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 3;
+                Armor = "";
+                Traits = "Blood Rage";
+                ActAbilities = "Rev,Chainsaw,Wild Slashes";
+                Upgrades = "Heavy Swing,Rile,Bloody Teeth";
+                break;
+            case "Painghoul/Freak":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 3;
+                Armor = "";
+                Traits = "Blood Rage";
+                ActAbilities = "Pain Frenzy,Cauterize,Meat Hook";
+                Upgrades = "Stim Hook,Frenzy Hook,Adrenalize";
+                break;
+            case "Painwheel/Horror":
+                Move = 5;
+                MaxHP = 4;
+                Defense = 3;
+                Armor = "";
+                Traits = "Blood Rage,Hellwheel";
+                ActAbilities = "Exfoliate,Spin Out";
+                Upgrades = "Barbed Wheel,Endless Screaming,Hurtle";
+                break;
+            case "Berserker/Tyrant":
+                Move = 4;
+                MaxHP = 6;
+                Defense = 2;
+                Armor = "";
+                Traits = "Blood Rage,Steaming Rage,Tyrant";
+                ActAbilities = "Pulverize,Building Rage,Rip and Tear";
+                Upgrades = "Machineheart,Fuel Injectors,Bifurcate";
+                break;
+            case "Warlord/Necromancer":
+                Move = 4;
+                MaxHP = 10;
+                Defense = 3;
+                Armor = "";
+                Traits = "Blood Rage,Fueled By Rage";
+                ActAbilities = "Superheated Chainblade";
+                SoulAbilities = "Ignite,Glory Kill";
+                break;
+            case "Scum/Thrall":
+                Move = 3;
+                MaxHP = 1;
+                Defense = 4;
+                Armor = "ARMOR";
+                Traits = "Toxic Revenge,Thrall";
+                ActAbilities = "Pseudopod,Shamble";
+                Upgrades = "Bloat,Tentacle Whip,Aftermath";
+                break;
+            case "Rotten/Scion":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 3;
+                Armor = "ARMOR";
+                Traits = "Plaguebearer";
+                ActAbilities = "Pustulate,Vomitous Mass,Rotblade";
+                Upgrades = "Catalyze,Invigorating Viscera,Smog Shroud";
+                break;
+            case "Leech/Freak":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "";
+                ActAbilities = "Suppurate,Evolve Strain,Swell with Corruption";
+                Upgrades = "Massive Swell,Spreading Strain,Acid Blood";
+                break;
+            case "Host/Hunter":
+                Move = 2;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Swarm Release";
+                ActAbilities = "Propagate Swarm,Driving Vermin";
+                Upgrades = "Swarm Feed,Toxic Avenger,Defiler";
+                break;
+            case "Slime/Horror":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Vile Rupture";
+                ActAbilities = "Percolate,Surge";
+                Upgrades = "Rotten Surge,Sticky Trail,The Gunk";
+                break;
+            case "Plaguelord/Necromancer":
+                Move = 4;
+                MaxHP = 10;
+                Defense = 4;
+                Armor = "";
+                Traits = "Blessed with Filth";
+                ActAbilities = "Virulence";
+                SoulAbilities = "Infest,Slime Form";
+                break;
+            case "Sacrifice/Thrall":
+                Move = 3;
+                MaxHP = 2;
+                Defense = 4;
+                Armor = "";
+                Traits = "Inverted Crucifix,Thrall";
+                ActAbilities= "Beckon,Shudder";
+                Upgrades = "Squirm,Dead Grasp,Impending Death";
+                break;
+            case "Chosen/Freak":
+                Move = 4;
+                MaxHP = 3;
+                Defense = 5;
+                Armor = "WARD";
+                Traits = "Slither";
+                ActAbilities = "Tombraiser,Kidnap,Serpent's Kiss";
+                Upgrades = "Ivory Monument,Leap,Foul Monuments";
+                break;
+            case "Vizigheist/Horror":
+                Move = 4;
+                MaxHP = 3;
+                Defense = 5;
+                Armor = "WARD";
+                Traits = "Teleport";
+                ActAbilities = "Horrendous Shriek,Urgal Blade";
+                Upgrades = "Terrorize,Soul Frost,Condemn";
+                break;
+            case "Banshee/Hunter":
+                Move = 3;
+                MaxHP = 3;
+                Defense = 5;
+                Armor = "WARD";
+                Traits = "Soul Sight";
+                ActAbilities = "Bale Scream,Tombstone";
+                Upgrades = "Doom Bell,Freeze Soul,Siren";
+                break;
+            case "Bound Devil/Tyrant":
+                Move = 3;
+                MaxHP = 6;
+                Defense = 3;
+                Armor = "WARD";
+                Traits = "Flight,Tyrant";
+                ActAbilities= "Omnipresence,Beckon Lamb,Limb From Limb";
+                Upgrades = "To The Slaughter,Death Toll,Strong Pact";
+                break;
+            case "Dark Priest/Necromancer":
+                Move = 4;
+                MaxHP = 8;
+                Defense = 4;
+                Armor = "WARD";
+                Traits = "Dread Presence";
+                ActAbilities = "Doom Blade";
+                SoulAbilities = "Cyclopean Monolith,Soulfeed";
+                break;
+            case "Penitent/Scion":
+                Move = 4;
+                MaxHP = 3;
+                Defense = 2;
+                Armor = "SUPER";
+                Traits = "Miracle";
+                ActAbilities = "Mea Culpa, Holy Water, Excoriate";
+                Upgrades = "Holy Blood, Eager, Taste the Lash";
+                break;
+            case "Zealot/Horror":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Miracle, Zealotry";
+                ActAbilities = "Smite, Whirling Chain";
+                Upgrades = "Punisher, Suffuse, Fiery Chain";
+                break;
+            case "Antipriest/Freak":
+                Move = 3;
+                MaxHP = 2;
+                Defense = 4;
+                Armor = "WARD";
+                Traits = "Miracle";
+                ActAbilities = "Gentleness, Delay Judgement, Blessed Censer";
+                Upgrades = "Cleansing Prayer, Consecrate, Boiling Censer";
+                break;
+            case "Inquisitor/Hunter":
+                Move = 3;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Miracle";
+                ActAbilities = "Winch, Requiesce en Pace";
+                Upgrades = "Focus, Heart Destroyer, Explosive Bolts";
+                break;
+            case "Holy Body/Tyrant":
+                Move = 4;
+                MaxHP = 3;
+                Defense = 2;
+                Armor = "SUPER";
+                Traits = "Flight, Miraculous Flesh, Tyrant";
+                ActAbilities = "Bolides, Indignation, Ablutions";
+                Upgrades = "Scathe, Holy Font, Witness";
+                break;
+            case "Exorcist/Necromancer":
+                Move = 4;
+                MaxHP = 10;
+                Defense = 4;
+                Armor = "";
+                Traits = "Holy Vessel";
+                ActAbilities = "Starmetal Godsword, Absolution, Will of God";
+                Upgrades = "";
+                break;
+            case "Stitch/Thrall":
+                Move = 3;
+                MaxHP = 2;
+                Defense = 4;
+                Armor = "";
+                Traits = "Fall To Shambles";
+                ActAbilities = "Unstable Mutation,Twisting Strike";
+                Upgrades = "Genestealer, Spread Mutate, Warping Mutate";
+                break;
+            case "Chop Doc/Freak":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "WARD";
+                Traits = "Leftovers";
+                ActAbilities = "Inject Mutagen, Purge, Marriage";
+                Upgrades = "Absorb, Scour Flesh, Conjoin";
+                break;
+            case "Lycan/Horror":
+                Move = 4;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Lope";
+                ActAbilities = "Bloodgorger, Bloody Slashes";
+                Upgrades = "The Hunger, Autophagia, Hunch";
+                break;
+            case "Strigoi/Hunter":
+                Move = 3;
+                MaxHP = 4;
+                Defense = 4;
+                Armor = "";
+                Traits = "Flight";
+                ActAbilities = "Regurgitate, Sin Eater";
+                Upgrades = "Drown In Viscera, Rapid Adaptation, Cleansing Wash";
+                break;
+            case "Homonculus/Tyrant":
+                Move = 4;
+                MaxHP = 6;
+                Defense = 2;
+                Armor = "";
+                Traits = "Warpflesh,Tyrant";
+                ActAbilities = "Scult Flesh, Absorb, Flesh Whip";
+                Upgrades = "Ball of Limbs, Meld, Form Carapace";
+                break;
+            case "Chiurgeon/Necromancer":
+                Move = 4;
+                MaxHP = 10;
+                Defense = 4;
+                Armor = "WARD";
+                Traits = "Polyglot";
+                ActAbilities = "Experimental Surgery, Wild Mutation, Sample Genome";
+                Upgrades = "";
+                break;
+            default:
+                throw new Exception($"UnitType '{unitType}' unsupported.");
+        }
     }
 
     public void UpdateSelectedTokenPanel() {
+        if (!TokenController.IsSelected(this)) {
+            return;
+        }
+
+        VisualElement panel = UI.System.Q("SelectedTokenPanel");
+
+        Color c = ClassColor();
+        panel.Q("ClassBackground").style.borderTopColor = c;
+        panel.Q("ClassBackground").style.borderRightColor = c;
+        panel.Q("ClassBackground").style.borderBottomColor = c;
+        panel.Q("ClassBackground").style.borderLeftColor = c;
+
+        panel.Q("Portrait").style.backgroundImage = Graphic;
+
+        panel.Q<Label>("CHP").text = CurrentHP.ToString();
+        panel.Q<Label>("MHP").text = "/" + MaxHP.ToString();
+        panel.Q<Label>("Name").text = Name;
+
+        panel.Q<ProgressBar>("HpBar").value = CurrentHP;
+        panel.Q<ProgressBar>("HpBar").highValue = MaxHP;
+
+        UI.ToggleDisplay(panel.Q<ProgressBar>("VigorBar"), false);
+        UI.ToggleDisplay(panel.Q<Label>("VIG"), false);
+        UI.ToggleDisplay(panel.Q("ResolveWrapper"), false);
+        UI.ToggleDisplay(panel.Q<Label>("Elite"), false);
+        for (int i = 1; i <= 4; i++) {
+            UI.ToggleDisplay(panel.Q("Wound" + i), false);
+        }
+
+
+        panel.Q<Label>("Job").text = UnitType;
+        panel.Q<Label>("Job").style.backgroundColor = c;
+
+        panel.Q("Statuses").Clear();
+        int statusCount = 0;
+        UI.ToggleDisplay("StatusColumn", statusCount > 0);
+
+
+        panel.Q<Label>("StatDef").text = Defense.ToString();
+        panel.Q<Label>("StatRng").text = Move.ToString();
+
     }
 
     public override bool CheckCondition(string label) {
