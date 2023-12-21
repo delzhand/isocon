ccf0013f847d764511e08bd13e6ce97a061fc62d
delzhand <delzhand@gmail.com>
2023-12-20 23:51:38 -0500

    world noise shader

---
+++ Assets/Scripts/UI/v0.6/Fader.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Fader : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+    }
+}
