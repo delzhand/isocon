a67c319a8e51f5ca320db60555f5ae689136270b
delzhand <delzhand@gmail.com>
2024-03-24 16:24:59 -0400

    bump version to 0.7.2

--- Assets/Scripts/System/Startup.cs
+++ Assets/Scripts/System/Startup.cs
@@ -35,35 +35,18 @@ public class Startup
 
     private static void ReleaseNotes()
     {
-        string version = "0.7.1";
-        string notes = @"<size=+3><b>IsoCON Version 0.7.1</b></size>
-
-<size=+2><b>Features</b></size>
-* Token Library - Maintain the tokens you plan to use in your games from inside Isocon and search it from the Add Token modal.
-* Animated Tokens - Frames of animation should be laid out horizontally, FPS and number of frames can be configured in the token library.
-* File Browser - tokens and maps can now be loaded from anywhere, not just the data directory.
-* A config option to keep block borders outside of editing/dragging has been added.
-* Homebrew support has been added - in the data directory you'll now find a folder called 'rules', and after launching this release it will contain a folder called latest.json. Copy this file and make your edits, and you can then select the new file in the config panel.
+        string version = "0.7.2";
+        string notes = @"<size=+3><b>IsoCON Version 0.7.2</b></size>
 
 <size=+2><b>Improvements</b></size>
-* Rebuilt token sync - speed and stability should be vastly improved
-* Data HUD added that shows information including the sync status of other players
-* The alt key can now be held when style painting blocks to quickly switch to the sample tool.
-* Dragging a token into the side of a block now automatically move it to the topmost block.
-* Eliminated hundreds of redundant function calls per frame.
-* Screen transitions implemented when moving between launcher and tabletop.
-* Camera can be rotated with right mouse button and dragged with middle mouse button. The top bar camera option now swaps this functionality.
-
-<size=+2><b>Changes</b></size>
-* Add Token button moved out of bottom bar, bottom bar hidden by default, shows now when the new Deploy button is clicked.
-* Info/Sync top bar options removed
+* Homebrew rule selection has been moved to the launcher
 
 <size=+2><b>Fixes</b></size>
-* Bugs with the dice roller have been resolved.
-* 6x8 Maleghast Map Maker maps now import correctly.
-
-<size=+2><b>Known Issues</b></size>
-* The change to token placement means that multi-level maps can't have tokens on a lower level anymore.";
+* Turn Advance button now functions correctly
+* Configured outline color is applied to new tokens
+* A bug preventing maps over a certain filesize from being shared has been resolved.
+* The Resolve/Party Resolve bar in ICON 1.5 token display has been fixed.
+* Long patch notes are now scrollable and shouldn't overflow the screen at certain resolution/scales";
 
         string seen = Preferences.GetReleaseNotesSeen();
         List<string> seenParts = seen.Split("|").ToList();
